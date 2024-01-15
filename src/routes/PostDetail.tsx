03534b218e4859583a6f3af3bf8a31f20e635cc4
solutione <solutione@solutione.co>
2024-01-15 17:23:11 +0900

    test

--- src/routes/PostDetail.tsx
+++ src/routes/PostDetail.tsx
@@ -2,7 +2,7 @@ import { useQuery } from "@tanstack/react-query";
 import { useParams } from "react-router-dom";
 import { getPostDetail } from "./api";
 import { IPostDetail } from "../types";
-import { Box, Grid, Heading, Image, Text } from "@chakra-ui/react";
+import { Box, Grid, HStack, Heading, Image, Text } from "@chakra-ui/react";
 
 export default function PostDetail() {
   const { postPk } = useParams();
@@ -11,9 +11,11 @@ export default function PostDetail() {
   <Box mt={10} px={{base:10, lg:40,}}>
     <Heading>{data?.title}</Heading>
     <Grid>
+        <Text>pulltest</Text>
         {data?.photo.map((photo) => (
             <Box key={photo.pk}>
                 <Image src={photo.photo_file} />
+                {data?.content}
             </Box>
         ))}
     </Grid>
