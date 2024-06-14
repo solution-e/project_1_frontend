8e9d5488e9955730aa6ccdecb17d26e7fcfafb1d
solution-e <154502155+solution-e@users.noreply.github.com>
2024-06-14 16:49:43 +0900

    addslash

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000/"
-      : "https://blur-3rcc.onrender.com",
+      : "https://blur-3rcc.onrender.com/",
   withCredentials: true,
 });
 
