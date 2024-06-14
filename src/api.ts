d7932b3f87245440a99b3cca69f03bb9bcf45666
solution-e <154502155+solution-e@users.noreply.github.com>
2024-06-14 10:04:19 +0900

    api

--- src/api.ts
+++ src/api.ts
@@ -7,7 +7,7 @@ const instance = axios.create({
   baseURL:
     process.env.NODE_ENV === "development"
       ? "http://127.0.0.1:8000/"
-      : "https://b-lur.com",
+      : "https://blur-3rcc.onrender.com",
   withCredentials: true,
 });
 
