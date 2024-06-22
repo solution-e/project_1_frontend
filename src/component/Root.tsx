ae64ff4d36e86c1a7090df9d9727973fa3a489a7
solution-e <154502155+solution-e@users.noreply.github.com>
2024-06-22 11:00:00 +0900

    meta edit4

--- src/component/Root.tsx
+++ src/component/Root.tsx
@@ -2,6 +2,7 @@ import { Box, useMediaQuery } from "@chakra-ui/react";
 import { Outlet } from "react-router-dom";
 import Header from "./Header";
 import SmartPhoneHeader from "./SmartPhoneHeader";
+import { HelmetProvider } from "react-helmet-async";
 
 export default function Root() {
   const [isSmartPhone] = useMediaQuery("(max-width: 768px)");
